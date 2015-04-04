120676b946aa07c46ad864346b7ce684653b7321
Oliver Fink <olifink@mac.com>
2015-04-04 17:12:26 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", function () {
+    console.log("Hello World!");
+
+    /*
+     if (window.Notification && Notification.permission !== "granted") {
+     Notification.requestPermission(function (status) {
+     if (Notification.permission !== status) {
+     Notification.permission = status;
+     }
+     });
+     }
+     */
+
+
+    var button = document.getElementsName('button');
+
+    button.addEventListener('click', function () {
+        var player = document.getElementById('player');
+        if(player.)
+        var n = new Notification("Hello World");
+
+    });
+
+});
