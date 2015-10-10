fdb486f6e3f30750cfd8081a1c19a1dff98097d9
Oliver Fink <olifink@mac.com>
2015-10-10 17:12:30 +0200

    fix

--- www/app.js
+++ www/app.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-    console.log("Hello World!");
+    console.log("Hello Worldy!");
 
     var player = document.getElementById('player');
     var buttonPlay = document.getElementById('buttonPlay');
